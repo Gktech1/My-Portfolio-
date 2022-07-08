2f30def36ff0e6f1d08c171284d2c2c1069d64aa
Edun Olukunle Elijah <favourblessing1@gmail.com>
2022-07-08 19:24:51 +0100

    Delete Profile directory

--- Profile/Models/Project.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Profile.Models
-{
-    public class Project
-    {
-        public int Id { get; set; }
-        public string ImageUrl { get; set; }
-        public string ProjectLink { get; set; }
-    }
-}
